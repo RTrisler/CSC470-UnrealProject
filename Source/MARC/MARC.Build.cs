c99b0c0f02211ab8d0e10dc4e15ba02c7196177b
Reid Trisler <reidtrisler@yahoo.com>
2022-12-13 00:57:14 -0600

    DefaultScene

---
+++ Source/MARC/MARC.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MARC : ModuleRules
+{
+	public MARC(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/MARC/MARCGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MARCGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMARCGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMARCGameMode();
+};
+
+
+
